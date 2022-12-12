340fd0c27236f68162755f6504e20f0fde77b3cd
rrudoyy <62769249+rrudoyy@users.noreply.github.com>
2022-12-12 08:23:49 +0200

    Add files via upload

---
+++ P168Ex14.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace ConsoleApp
+{
+
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            int amount = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (IsPerfect(i))
+                    amount++;
+            }
+
+            Console.WriteLine(amount);
+        }
+
+        public static bool IsPerfect(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine(num);
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+}
