f964884d4f1c3c6edd4e9c863ac7be6ca6cb3adc
rrudoyy <62769249+rrudoyy@users.noreply.github.com>
2022-12-12 08:09:55 +0200

    Add files via upload

---
+++ P167Ex12.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ConsoleApp
+{
+
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            int id = 0, grade = 0;
+            bool finishGrades = false;
+            Student student;
+
+            List<Student> students = new List<Student>();
+
+            do
+            {
+                Console.WriteLine("Enter the student ID:");
+                id = int.Parse(Console.ReadLine());
+                if (id == -1)
+                    break;
+
+                student = new Student(id);
+                while (!finishGrades)
+                {
+                    Console.WriteLine("Enter the student grade:");
+                    grade = int.Parse(Console.ReadLine());
+                    if (grade == -999)
+                        break;
+                    else
+                        student.AddGrade(grade);
+                }
+
+                students.Add(student);
+
+            } while (id != -1);
+
+            Console.WriteLine("The amount of students - " + students.Count);
+            students.ForEach(s => Console.WriteLine("Student #" + s.GetID() + " - " + s.GetGrades().Average()));
+            Console.WriteLine(students.Where(s => s.GetGrades()[0] >= 90).Count());
+
+        }
+
+    }
+
+    public class Student
+    {
+        private int Id;
+        public List<int> grades;
+
+        public Student(int Id)
+        {
+            this.Id = Id;
+            this.grades = new List<int>();
+        }
+
+        public void AddGrade(int grade)
+        {
+            grades.Add(grade);
+        }
+
+        public int GetID()
+        {
+            return Id;
+        }
+        public List<int> GetGrades()
+        {
+            return grades;
+        }
+    }
+
+}
